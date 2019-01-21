a04ea853b6d79e69e9b6e2bb54c9ef8bcacfa53d
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-21 19:34:18 +0000

    added user input functionality

--- lib/top_movies/cli.rb
+++ lib/top_movies/cli.rb
@@ -1,15 +1,18 @@
 class CLI
 
+  attr_accessor :sorted_movies
+
   def start
-    puts "Welcome to the Top Rated Movies of 2018-2019!"
+    puts "Welcome to the Top 20 Rated Movies of 2018-2019!"
     puts "What movie would you like to view?"
     Scraper.scrape_movies
-
-    #display list of movies to users
+  #display list of movies to users
     list_movies
+    get_movie
   end
 
   def list_movies
+
     all_movies = Movie.all_movies
     #binding.pry
     all_movies.each do |movie|
@@ -20,4 +23,25 @@ class CLI
     end
   end
 
+  def get_movie
+    puts "Please enter the title of the movie you wish to view:"
+    input = gets.chomp.downcase
+    all_movies = Movie.all_movies
+    #all_movies.detect { |movie| movie.title.downcase == input}
+    movie_choice = all_movies.detect { |movie| movie.title.downcase == input}
+    binding.pry
+    if input == '#{movie.title}'
+      puts "#{movie.title}. #{movie.overview}. #{movie.year}"
+    elsif input == nil
+      puts "Please enter a valid title:"
+    elsif input == "exit"
+      goodbye
+  end
 end
+end
+
+
+
+def goodbye
+    puts "Thank you for viewing the top 20 movies!!!"
+  end
