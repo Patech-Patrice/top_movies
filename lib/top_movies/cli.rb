cbca9046384e83cb9cbad0d2c778ca5b1cad9d66
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-23 01:30:38 +0000

    changed user input and output flow

--- lib/top_movies/cli.rb
+++ lib/top_movies/cli.rb
@@ -26,16 +26,30 @@ class CLI
       #find the movie
       #2nd scrape
       #list the movie that corresponds with the input number
-    elsif
-      input == "list"
-      list_movies
+    #elsif
+      #input == "list"
+      #list_movies
     elsif input == "exit"
-      puts "Goodbye"
+      farewell
     else
-      puts "Invalid choice"
+      puts "Invalid choice...."
+      #list_movies
       get_movie #recursive method ---calling the same method from inside of the method
+      start
+
     all_movies = Movie.all_movies
     all_movies.each.with_index(1) {|movie, index| puts "#{index} #{movie.title} "}
-  end
+ end
 end
+
+
+  def farewell
+    puts "Would you like to try a different number movie? (y/n)"
+    input = gets.strip.downcase
+    if input == 'y' || input == 'yes'
+      start
+    else input = 'n' || input == 'no'
+      puts "Thank you for viewing the Top 100 Hollywood Films of all Time.Goodbye!"
+    end
+  end
 end
